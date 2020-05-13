27b774547e2f429a1a5a2e44251929c354bcb795
chloe-codes1 <juhyun.kim@lindsey.edu>
2020-05-13 10:33:00 +0900

    Add admin.py

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Article, Comment, Hashtag
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'content', 'image', 'image_thumbnail', 'created_at', 'updated_at', 'user', )
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('content', 'article', 'user', 'created_at', 'parent',)
+
+class HashtagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Hashtag, HashtagAdmin)
